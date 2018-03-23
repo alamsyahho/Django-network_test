46f9b11e06795edd2d3cce237e8ac81c05e4dd40
root <root@uat-r7-zabbix1.uat.iac-nets.sg>
2018-03-23 17:52:12 +0800

    add more detail for test result

--- network_test/libs/zabbix.py
+++ network_test/libs/zabbix.py
@@ -9,20 +9,20 @@ class zabbix_check(object):
         self.port         = port
 
     def _zabbix_get_tcp(self, source, destination, port):
-        tcp_output = subprocess.check_output(["zabbix_get", "-s", source, "-k", "net.tcp.port[" + destination + "," + port + "]"])
+        tcp_output = subprocess.check_output(["zabbix_get", "-s", source, "-k", "net.tcp.port[" + destination + "," + port + "]"]).strip()
         return tcp_output
 
     def _zabbix_get_ntp(self, source, destination, port):
-        ntp_output = subprocess.check_output(["zabbix_get", "-s", source, "-k", "net.udp.service[ntp," + destination + "," + port + "]"])
+        ntp_output = subprocess.check_output(["zabbix_get", "-s", source, "-k", "net.udp.service[ntp," + destination + "," + port + "]"]).strip()
         return ntp_output
 
     def _zabbix_get_dns(self, source, destination, port):
-        dns_output = subprocess.check_output(["zabbix_get", "-s", source, "-k", "net.dns[" + destination + "]"])
+        dns_output = subprocess.check_output(["zabbix_get", "-s", source, "-k", "net.dns[" + destination + "]"]).strip()
         return dns_output
 
     def _zabbix_agent_ping(self, source):
         try:
-            subprocess.check_output(["zabbix_get", "-s", source, "-k", "agent.ping"])
+            subprocess.check_output(["zabbix_get", "-s", source, "-k", "agent.ping"]).strip()
             return "agent alive"
         except:
             return source + " is unreachable or don't have zabbix agent installed"
@@ -41,4 +41,9 @@ class zabbix_check(object):
         elif self.protocol == 'dns':
             result = self._zabbix_get_dns(self.source, self.destination, self.port)
 
-        return result
+	if result == '1':
+	    return "Success! Connection established successfully from " + self.source + " to " + self.destination + " on " + self.protocol + " port " + self.port 
+	elif result == '0':
+	    return "Failed! Cannot connect from " + self.source + " to " + self.destination + " on " + self.protocol + " port " + self.port
+	else:
+	    return "abcd"
